f37f89775acb137fb4f789bf31d9420e38feb0b4
thunderbiscuit <thunderbiscuit@protonmail.com>
2024-01-22 19:43:49 -0500

    Add FountainDecoder type

---
+++ lib/src/main/kotlin/org/kotlinbitcointools/ur/fountain/FountainDecoder.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2023 thunderbiscuit and contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the ./LICENSE.txt file.
+ */
+
+package org.kotlinbitcointools.ur.fountain
+
+public class FountainDecoder {
+    public fun joinFragments(fragments: List<ByteArray>, messageLength: Int): ByteArray {
+        val combinedFragments: List<Byte> = fragments.flatMap { it.toList() }.take(messageLength)
+        return combinedFragments.toByteArray()
+    }
+}
