7b041f4dbb81b9c2311054a80855ded9aed87774
thunderbiscuit <thunderbiscuit@protonmail.com>
2024-02-12 10:15:50 -0500

    Update license notice years range

--- src/main/kotlin/org/kotlinbitcointools/ur/registry/PSBT.kt
+++ src/main/kotlin/org/kotlinbitcointools/ur/registry/PSBT.kt
@@ -1,13 +1,9 @@
 /*
- * Copyright 2023 thunderbiscuit and contributors.
+ * Copyright 2023-2024 thunderbiscuit and contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the ./LICENSE.txt file.
  */
 
 package org.kotlinbitcointools.ur.registry
-
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.dataformat.cbor.CBORFactory
-import org.kotlinbitcointools.ur.UR
 
 // public class PSBT(
 //     public val psbt: Psbt,
