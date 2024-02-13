ecd3db961e6cad8af86bb8713543d7716d207be2
thunderbiscuit <thunderbiscuit@protonmail.com>
2024-02-13 10:58:39 -0500

    Add longer input bytewords encoding test

--- src/test/kotlin/org/kotlinbitcointools/ur/BytewordsTest.kt
+++ src/test/kotlin/org/kotlinbitcointools/ur/BytewordsTest.kt
@@ -76,4 +76,27 @@ class BytewordsTest {
             Bytewords.decode("ma", Bytewords.Style.MINIMAL)
         }
     }
+
+    @Test
+    fun `Bigger input`() {
+        val bytes = listOf(
+            245, 215, 20, 198, 241, 235, 69, 59, 209, 205, 165, 18, 150, 158, 116, 135, 229, 212, 19, 159,
+            17, 37, 239, 240, 253, 11, 109, 191, 37, 242, 38, 120, 223, 41, 156, 189, 242, 254, 147, 204,
+            66, 163, 216, 175, 191, 72, 169, 54, 32, 60, 144, 230, 210, 137, 184, 197, 33, 113, 88, 14,
+            157, 31, 177, 46, 1, 115, 205, 69, 225, 150, 65, 235, 58, 144, 65, 240, 133, 69, 113, 247,
+            63, 53, 242, 165, 160, 144, 26, 13, 79, 237, 133, 71, 82, 69, 254, 165, 138, 41, 85, 24
+        )
+        val input = bytes.map { it.toByte() }.toByteArray()
+        val expectedEncoded = "yank toys bulb skew when warm free fair tent swan open brag mint noon jury list view tiny brew note body data webs what zinc bald join runs data whiz days keys user diet news ruby whiz zone menu surf flew omit trip pose runs fund part even crux fern math visa tied loud redo silk curl jugs hard beta next cost puma drum acid junk swan free very mint flap warm fact math flap what limp free jugs yell fish epic whiz open numb math city belt glow wave limp fuel grim free zone open love diet gyro cats fizz holy city puff"
+        val expectedEncodedMinimal = "yktsbbswwnwmfefrttsnonbgmtnnjyltvwtybwnebydawswtzcbdjnrsdawzdsksurdtnsrywzzemusffwottppersfdptencxfnmhvatdldroskcljshdbantctpadmadjksnfevymtfpwmftmhfpwtlpfejsylfhecwzonnbmhcybtgwwelpflgmfezeonledtgocsfzhycypf"
+
+        assertEquals(
+            expected = expectedEncoded,
+            actual = Bytewords.encode(input, Bytewords.Style.STANDARD)
+        )
+        assertEquals(
+            expected = expectedEncodedMinimal,
+            actual = Bytewords.encode(input, Bytewords.Style.MINIMAL)
+        )
+    }
 }
